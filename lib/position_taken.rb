605cbd0c3fbe5e76740fbb403e4b35f5827b51c5
Marianne Pyon <mariannepyon@gmail.com>
2019-07-25 18:18:00 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+def position_taken?(board, location)
+  index = board[location.to_i - 1]
+
+  if index != " "
+    "false"
+  elsif index != ""
+    "false"
+  elsif index != nil
+    "false"
+  else index == "X" or "O"
+    "true"
+
+end
