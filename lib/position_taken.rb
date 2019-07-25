8ae9c645922dc76287d777b7805718bcae6cbaf7
Marianne Pyon <mariannepyon@gmail.com>
2019-07-25 17:53:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,14 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+  array[index_number]
+
+  if index == " "
+    "false"
+  elsif index == ""
+    "false"
+  elsif index == nil
+    "false"
+  else index == X or O
+    "true"
 
 end
